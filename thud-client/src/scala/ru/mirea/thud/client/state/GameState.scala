9d2ab5ad35d4f0e3dfa864578edc346fd160ff1c
Cancer <mizantrop2397@gmail.com>
2018-03-10 17:25:13 +0300

    player state refactoring

--- thud-client/src/scala/ru/mirea/thud/client/state/GameState.scala
+++ thud-client/src/scala/ru/mirea/thud/client/state/GameState.scala
@@ -7,10 +7,10 @@ import ru.mirea.thud.common.constants.CommonConstants.EMPTY_STRING
 
 case class PlayerState(var id: String = EMPTY_STRING, var name: String = EMPTY_STRING, var score: Int = 0, var role: PlayerRole.Value = TROLL)
 
-case class Field(units: List[GameUnit] = List())
+case class GameField(units: List[GameUnit] = List())
 
 case class GameScore(var playerVictories: Int = 0, var enemyPlayerVictories: Int = 0)
 
-case class GameState(var playerState: PlayerState = PlayerState(), var enemyPlayerState: PlayerState = PlayerState(), var gameScore: GameScore = GameScore(), var field: Field = Field())
+case class GameState(var playerState: PlayerState = PlayerState(), var enemyPlayerState: PlayerState = PlayerState(), var gameScore: GameScore = GameScore(), var field: GameField = GameField())
 
 object GameState extends GameState

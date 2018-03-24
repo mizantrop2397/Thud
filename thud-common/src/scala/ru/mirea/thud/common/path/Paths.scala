e511dae4184223f2dce3b2a6ccb3a423d37a2567
Cancer <mizantrop2397@gmail.com>
2018-03-25 02:50:16 +0300

    added main server logic

--- thud-common/src/scala/ru/mirea/thud/common/path/Paths.scala
+++ thud-common/src/scala/ru/mirea/thud/common/path/Paths.scala
@@ -1,9 +1,9 @@
 package ru.mirea.thud.common.path
 
-import ru.mirea.thud.common.constants.CommonConstants.{HOST, SERVER_ACTOR_SYSTEM, SERVER_PORT}
+import ru.mirea.thud.common.constants.CommonConstants._
 
 object Paths {
   def serverActorPath(actorName: String) = s"akka.tcp://$SERVER_ACTOR_SYSTEM@$HOST:$SERVER_PORT/user/$actorName"
 
-  def clientActorPath(actorName: String, clientPort: Int, clientHost: String) = s"akka.tcp://$SERVER_ACTOR_SYSTEM@$clientHost:$clientPort/user/$actorName"
+  def clientActorPath(actorName: String, clientPort: Int, clientHost: String) = s"akka.tcp://$CLIENT_ACTOR_SYSTEM@$clientHost:$clientPort/user/$actorName"
 }

5911c6cf644709ee161bd86b855739f846616eb3
Cancer <mizantrop2397@gmail.com>
2018-03-25 02:00:54 +0300

    server logic big commit..

--- thud-server/src/scala/ru/mirea/thud/server/controller/GameService.scala
+++ thud-server/src/scala/ru/mirea/thud/server/controller/GameService.scala
@@ -3,40 +3,49 @@ package ru.mirea.thud.server.controller
 import java.util.UUID.randomUUID
 
 import akka.actor.Actor
-import ru.mirea.thud.common.messages.{DrawOfferingMessage, PlayerActionNotificationMessage, PlayerConnectionMessage, PlayerDisconnectionMessage}
-import ru.mirea.thud.common.model.GameField
-import ru.mirea.thud.server.session.PlayerSession
+import ru.mirea.thud.common.messages._
+import ru.mirea.thud.common.model.{GameField, PlayerConnectionInfo}
+import ru.mirea.thud.server.app.Server.clientPlayerService
+import ru.mirea.thud.server.session.{PlayerInfo, PlayerSession}
 
 import scala.collection.mutable
 
 class GameService extends Actor {
   val connectedPlayers: mutable.HashMap[String, PlayerSession] = new mutable.HashMap[String, PlayerSession]
 
-  val playersWaitingQueue: mutable.Queue[String] = new mutable.Queue[String]
+  val playersWaitingQueue: mutable.Queue[PlayerConnectionInfo] = new mutable.Queue[PlayerConnectionInfo]
 
   val gameField = new GameField
 
+
   override def receive: Receive = {
-    case PlayerConnectionMessage(playerName) => handleNewPlayer(playerName)
-    case PlayerDisconnectionMessage(playerName) => disconnectPlayer(playerName)
+    case PlayerConnectionMessage(playerInfo) => handleNewPlayer(playerInfo)
+    case PlayerDisconnectionMessage(sessionId) => disconnectPlayer(sessionId)
     case PlayerActionNotificationMessage() => notifyEnemyAboutPlayerAction()
-    case DrawOfferingMessage() => drawOfferToEnemyPlayer()
+    case DrawOfferingMessageToServer(sessionId, playerId) => offerDrawToEnemyPlayer(sessionId, playerId)
   }
 
-  def drawOfferToEnemyPlayer(): Unit = ???
-
-  def notifyEnemyAboutPlayerAction(): Unit = ???
-
-  def disconnectPlayer(playerName: String): Unit = {
-    connectedPlayers -= playerName
-  }
-
-  def handleNewPlayer(playerName: String): Unit = {
+  def handleNewPlayer(info: PlayerConnectionInfo): Unit = {
     if (playersWaitingQueue.nonEmpty) {
       val sessionId = randomUUID().toString
-      connectedPlayers += sessionId -> PlayerSession(sessionId, playersWaitingQueue.front, playerName)
+      val firstPlayerId = randomUUID().toString
+      val secondPlayerId = randomUUID().toString
+      connectedPlayers += sessionId -> PlayerSession(sessionId, PlayerInfo(firstPlayerId, playersWaitingQueue.front), PlayerInfo(secondPlayerId, info))
       return
     }
-    playersWaitingQueue += playerName
+    playersWaitingQueue += info
+  }
+
+  def offerDrawToEnemyPlayer(sessionId: String, playerId: String): Unit = {
+    val enemyPlayerInfo = connectedPlayers(sessionId) getPlayer playerId
+    clientPlayerService(enemyPlayerInfo.connectionInfo.port, enemyPlayerInfo.connectionInfo.host) ! DrawOfferingMessageToClient()
+  }
+
+  def notifyEnemyAboutPlayerAction(): Unit = {
+
+  }
+
+  def disconnectPlayer(sessionId: String): Unit = {
+    connectedPlayers -= sessionId
   }
 }

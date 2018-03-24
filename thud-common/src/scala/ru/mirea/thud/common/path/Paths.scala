3c920cf1c7649e5d02c6cd3706eec3e61c5fc66d
Cancer <mizantrop2397@gmail.com>
2018-03-25 02:56:26 +0300

    constants refactoring

--- thud-common/src/scala/ru/mirea/thud/common/path/Paths.scala
+++ thud-common/src/scala/ru/mirea/thud/common/path/Paths.scala
@@ -3,7 +3,7 @@ package ru.mirea.thud.common.path
 import ru.mirea.thud.common.constants.CommonConstants._
 
 object Paths {
-  def serverActorPath(actorName: String) = s"akka.tcp://$SERVER_ACTOR_SYSTEM@$HOST:$SERVER_PORT/user/$actorName"
+  def serverActorPath(actorName: String) = s"akka.tcp://$SERVER_ACTOR_SYSTEM@$SERVER_HOST:$SERVER_PORT/user/$actorName"
 
   def clientActorPath(actorName: String, clientPort: Int, clientHost: String) = s"akka.tcp://$CLIENT_ACTOR_SYSTEM@$clientHost:$clientPort/user/$actorName"
 }

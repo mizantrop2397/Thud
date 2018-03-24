c28f60b637b9988e43ca7fd5d2d4dc63e183f8a9
Cancer <mizantrop2397@gmail.com>
2018-03-25 02:50:16 +0300

    added main server logic

--- thud-client/src/scala/ru/mirea/thud/client/state/GameState.scala
+++ thud-client/src/scala/ru/mirea/thud/client/state/GameState.scala
@@ -1,14 +1,10 @@
 package ru.mirea.thud.client.state
 
-import ru.mirea.thud.common.constants.CommonConstants.EMPTY_STRING
-import ru.mirea.thud.common.constants.PlayerRole
-import ru.mirea.thud.common.constants.PlayerRole.TROLL
-import ru.mirea.thud.common.model.GameField
+import ru.mirea.thud.common.model.{GameField, PlayerState}
 
-case class PlayerState(var id: String = EMPTY_STRING, var name: String = EMPTY_STRING, var score: Int = 0, var role: PlayerRole.Value = TROLL)
 
 case class GameScore(var playerVictories: Int = 0, var enemyPlayerVictories: Int = 0)
 
-case class GameState(var playerState: PlayerState = PlayerState(), var enemyPlayerState: PlayerState = PlayerState(), var gameScore: GameScore = GameScore(), var field: GameField = GameField())
+case class GameState(var playerState: PlayerState = null, var enemyPlayerState: PlayerState = null, var gameScore: GameScore = GameScore(), var field: GameField = GameField())
 
-object GameState extends GameState(playerState = PlayerState(), enemyPlayerState = PlayerState(), gameScore = GameScore(), field = GameField())
+object GameState extends GameState(playerState = null, enemyPlayerState = null, gameScore = GameScore(), field = GameField())

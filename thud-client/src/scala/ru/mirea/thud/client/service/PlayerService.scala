5dd7feb44222fd1f6906bf2a8514d68add08bf29
Anton.Bashirov <anton.bashirov@rt.ru>
2018-03-27 23:34:41 +0300

    Комменты по ревью

--- thud-client/src/scala/ru/mirea/thud/client/service/PlayerService.scala
+++ thud-client/src/scala/ru/mirea/thud/client/service/PlayerService.scala
@@ -1,6 +1,7 @@
 package ru.mirea.thud.client.service
 
 import java.util
+import java.util.stream.Collectors
 
 import akka.actor.Actor
 import ru.mirea.thud.client.constants.CellTargetMode
@@ -10,9 +11,13 @@ import ru.mirea.thud.client.model.FieldUnit
 import ru.mirea.thud.client.model.messages.{AttackMessage, CalculateMovementSchemeMessage, HighlightCellsMessage, MovementMessage}
 import ru.mirea.thud.common.model.Location
 
+/**
+  * Review:
+  * 1. Почему все protected? Можно ж private
+  * 2. А почему нельзя сделать для fieldController-а сообщение ResetHighlightedCells вместо вызова  HighlightCellsMessage с дефолтными параметарми
+  */
 class PlayerService extends Actor {
   override def receive: Receive = {
-
     case CalculateMovementSchemeMessage(unit) => unit.cellType match {
       case DWARF => DwarfService.calculateDwarfMovement(unit)
       case TROLL => TrollService.calculateTrollMovement(unit)
@@ -22,7 +27,7 @@ class PlayerService extends Actor {
       processMovement(unit, newCell)
       unit.cellType match {
         case DWARF => //TODO: Миша, это твое! Пиши давай, ленивая задница, а то сожгу!
-        case TROLL => setHighlightedCellsToDefault(TrollService.getCellsToHighlightAttack, TrollService.getCellsToHighlightMove)
+        case TROLL => setHighlightedCellsToDefault(TrollService.getCellsToHighlightAttack, TrollService.getCellsToHighlightMove) // Review. Предлагаю переименовать в 'resetHighlightedCells'
       }
 
     case AttackMessage(attackedUnit) => attackedUnit.cellType match {
@@ -35,19 +40,18 @@ class PlayerService extends Actor {
     Move a figure
   */
   protected def processMovement(unit: FieldUnit, newCell: FieldUnit): Unit = {
-    unit.location.copy(newCell.location.x, newCell.location.y)
+    unit.location.copy(newCell.location.x, newCell.location.y) // Здесь будет вызов сервака (впилит Антон)
   }
 
   /*
   Convert ArrayList to Map [Location, CellTargetMode]
 */
   protected def addToMap(fieldUnit: util.ArrayList[FieldUnit], cellTargetMode: CellTargetMode.Value): Unit = {
-    var map : Map[Location, CellTargetMode.Value] = Map()
-    for (i <- 0 to fieldUnit.size())
-    {
+    var map: Map[Location, CellTargetMode.Value] = Map()
+    for (i <- 0 to fieldUnit.size()) {
       map += (fieldUnit.get(i).location -> cellTargetMode)
     }
-    HighlightCellsMessage (map)
+    HighlightCellsMessage(map) //Review. Не хватает актера, которому сообщение отправляется. По идее ThudGame.fieldController ! HighlightCellsMessage(map)
   }
 
   protected def setHighlightedCellsToDefault(arrayToAttack: util.ArrayList[FieldUnit], arrayToMove: util.ArrayList[FieldUnit]): Unit = {

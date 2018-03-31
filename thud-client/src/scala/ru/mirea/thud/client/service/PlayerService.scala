616a3225dd4b37392182ce987ffdf613cea2db4e
Jaypaw <voinverecka@mail.ru>
2018-03-31 19:05:44 +0300

    Правки по ревью + отправка сообщений

--- thud-client/src/scala/ru/mirea/thud/client/service/PlayerService.scala
+++ thud-client/src/scala/ru/mirea/thud/client/service/PlayerService.scala
@@ -4,12 +4,13 @@ import java.util
 import java.util.stream.Collectors
 
 import akka.actor.Actor
+import ru.mirea.thud.client.app.ThudGame
+import ru.mirea.thud.client.constants.FieldCellType._
 import ru.mirea.thud.client.constants.CellTargetMode
 import ru.mirea.thud.client.constants.CellTargetMode.DEFAULT
 import ru.mirea.thud.common.constants.FieldCellType._
 import ru.mirea.thud.client.model.FieldUnit
-import ru.mirea.thud.client.model.messages.{AttackMessage, CalculateMovementSchemeMessage, HighlightCellsMessage, MovementMessage}
-import ru.mirea.thud.common.model.Location
+import ru.mirea.thud.client.model.messages._
 
 /**
   * Review:
@@ -17,20 +18,22 @@ import ru.mirea.thud.common.model.Location
   * 2. А почему нельзя сделать для fieldController-а сообщение ResetHighlightedCells вместо вызова  HighlightCellsMessage с дефолтными параметарми
   */
 class PlayerService extends Actor {
+  def action : CommonUnitActions = new CommonUnitActions()
+
   override def receive: Receive = {
     case CalculateMovementSchemeMessage(unit) => unit.cellType match {
       case DWARF => DwarfService.calculateMovement(unit)
       case TROLL => TrollService.calculateTrollMovement(unit)
     }
 
-    case MovementMessage(unit, newCell) =>
+    case PerformMovementMessage(unit, newCell) =>
       processMovement(unit, newCell)
       unit.cellType match {
         case DWARF => //TODO: Миша, это твое! Пиши давай, ленивая задница, а то сожгу!
         case TROLL => setHighlightedCellsToDefault(TrollService.getCellsToHighlightAttack, TrollService.getCellsToHighlightMove) // Review. Предлагаю переименовать в 'resetHighlightedCells'
       }
 
-    case AttackMessage(attackedUnit) => attackedUnit.cellType match {
+    case PerformAttackMessage(attackedUnit) => attackedUnit.cellType match {
       case DWARF => DwarfService.processAttack(attackedUnit)
       case TROLL => TrollService.processTrollAttack(attackedUnit)
     }
@@ -39,24 +42,9 @@ class PlayerService extends Actor {
   /*
     Move a figure
   */
-  protected def processMovement(unit: FieldUnit, newCell: FieldUnit): Unit = {
-    unit.location.copy(newCell.location.x, newCell.location.y) // Здесь будет вызов сервака (впилит Антон)
+  private def processMovement(unit: FieldUnit, newCell: FieldUnit): Unit = {
+    unit.location.copy(newCell.location.x, newCell.location.y)
   }
 
-  /*
-  Convert ArrayList to Map [Location, CellTargetMode]
-*/
-  protected def addToMap(fieldUnit: util.ArrayList[FieldUnit], cellTargetMode: CellTargetMode.Value): Unit = {
-    var map: Map[Location, CellTargetMode.Value] = Map()
-    for (i <- 0 to fieldUnit.size()) {
-      map += (fieldUnit.get(i).location -> cellTargetMode)
-    }
-    HighlightCellsMessage(map) //Review. Не хватает актера, которому сообщение отправляется. По идее ThudGame.fieldController ! HighlightCellsMessage(map)
-  }
-
-  protected def setHighlightedCellsToDefault(arrayToAttack: util.ArrayList[FieldUnit], arrayToMove: util.ArrayList[FieldUnit]): Unit = {
-    addToMap(arrayToAttack, DEFAULT)
-    addToMap(arrayToMove, DEFAULT)
-  }
 }
 
--- thud-client/src/scala/ru/mirea/thud/client/service/TrollService.scala
+++ thud-client/src/scala/ru/mirea/thud/client/service/TrollService.scala
@@ -2,10 +2,11 @@ package ru.mirea.thud.client.service
 
 import java.util
 
+import ru.mirea.thud.client.app.ThudGame
 import ru.mirea.thud.client.constants.CellTargetMode
-import ru.mirea.thud.common.constants.FieldCellType.{DWARF, EMPTY, TROLL}
+import ru.mirea.thud.client.constants.FieldCellType.{DWARF, EMPTY, TROLL}
 import ru.mirea.thud.client.constants.CellTargetMode.{ATTACK, MOVE}
-import ru.mirea.thud.client.model.messages.HighlightCellsMessage
+import ru.mirea.thud.client.model.messages.{DeleteFiguresMessage, HighlightCellsMessage, DeleteFiguresMessage}
 import ru.mirea.thud.client.model.{Cell, FieldUnit}
 import ru.mirea.thud.common.model.Location
 
@@ -15,65 +16,47 @@ Order of neighbors in list (numbers = indexes)
 3  X  1
 7  2  6
 */
-object TrollService {
-  /**  Review. Для чего используются эти свойства кроме как setHighlightedCellsToDefault ? **/
+object TrollService{
+
   private var cellsToHighlightAttack = new util.ArrayList[FieldUnit]
   private var cellsToHighlightMove = new util.ArrayList[FieldUnit]
 
-  def getCellsToHighlightAttack: util.ArrayList[FieldUnit] = { // Review. Можно короче:   def getCellsToHighlightAttack: util.ArrayList[FieldUnit] = cellsToHighlightAttack
-    cellsToHighlightAttack
-  }
-
-  def getCellsToHighlightMove: util.ArrayList[FieldUnit] = {
-    cellsToHighlightMove
-  }
-
+  def getCellsToHighlightAttack: util.ArrayList[FieldUnit] = cellsToHighlightAttack
+  def getCellsToHighlightMove: util.ArrayList[FieldUnit] = cellsToHighlightMove
   /*
     Calculate possible moves
   */
   def calculateTrollMovement(controllingUnit: FieldUnit): Unit = {
+    def action : CommonUnitActions = new CommonUnitActions()
     cellsToHighlightAttack = checkForTrollsAttack(controllingUnit)
-    addToMap(cellsToHighlightAttack, ATTACK)
     cellsToHighlightMove = checkForTrollsMovement(controllingUnit)
-    addToMap(cellsToHighlightMove, MOVE)
+    var map : Map[Location, CellTargetMode.Value] = action.addToMap(cellsToHighlightAttack, ATTACK)
+    map.++(action.addToMap(cellsToHighlightMove, MOVE))
+    ThudGame.fieldController ! HighlightCellsMessage (map)
   }
 
-  /*
-    Setting cells with dwarfs to empty and notify other player
-  */
-  def processTrollAttack(selectedCell: FieldUnit): Unit = {
-    val dwarfsToKill = new util.ArrayList[FieldUnit]()
-    for (neighbor <- selectedCell.neighbors if neighbor.cellType.equals(DWARF)) {
-      neighbor.cellType = EMPTY
-      dwarfsToKill.add(neighbor)
-    }
-    if (!dwarfsToKill.isEmpty) {
-      //send message
-    }
-  }
-
-  /*
-     Check cells for attack possibility
-       return ArrayList
-  */
+ /*
+    Check cells for attack possibility
+      return ArrayList
+ */
   private def checkForTrollsAttack(controllingUnit: FieldUnit): util.ArrayList[FieldUnit] = {
-    val possibleCells = new util.ArrayList[FieldUnit]()
-    (0 to 3).foreach {
+    var possibleCells = new util.ArrayList[FieldUnit]()
+    (0 to 3).foreach{
       i => {
         var count = 0
         val neighbor = controllingUnit.neighbors(i)
-        if (Cell.isCellTroll(neighbor) && ((i < 2 && controllingUnit.neighbors(i + 2).cellType.equals(TROLL)) && (i > 2 && controllingUnit.neighbors(i - 2).cellType.equals(TROLL)))) {
+        if (Cell.isCellTroll(neighbor) && ((i < 2 && controllingUnit.neighbors(i+2).cellType.equals(TROLL)) && (i > 2 && controllingUnit.neighbors(i-2).cellType.equals(TROLL)))){
           count = countLineLength(neighbor, i)
         }
         var index = i
-        if (i < 2) {
+        if (i < 2){
           index = index + 2
         } else {
           index = index - 2
         }
-        (0 to count).foreach {
+        (0 to count).foreach{
           j => {
-            if (controllingUnit.neighbors(index).cellType.equals(EMPTY)) {
+            if (controllingUnit.neighbors(index).cellType.equals(EMPTY)){
               possibleCells.add(controllingUnit.neighbors(index))
             }
             controllingUnit.copy(controllingUnit.neighbors(index).location, controllingUnit.neighbors(index).cellType, controllingUnit.neighbors(index).neighbors)
@@ -84,23 +67,12 @@ object TrollService {
     possibleCells
   }
 
-  private def countLineLength(currentUnit: FieldUnit, index: Int): Int = {
+  private def countLineLength (currentUnit: FieldUnit, index: Int): Int = {
     var count = 0
-    if (currentUnit.neighbors(index).cellType.equals(TROLL)) {
+    if (currentUnit.neighbors(index).cellType.equals(TROLL)){
       count = countLineLength(currentUnit.neighbors(index), index)
     }
     count + 1
-  }
-
-  /*
-    Convert ArrayList to Map [Location, CellTargetMode]
-  */
-  private def addToMap(fieldUnit: util.ArrayList[FieldUnit], cellTargetMode: CellTargetMode.Value): Unit = {
-    var map: Map[Location, CellTargetMode.Value] = Map()
-    for (i <- 0 to fieldUnit.size()) {
-      map += (fieldUnit.get(i).location -> cellTargetMode)
-    }
-    HighlightCellsMessage(map) // Review. Забыли контроллер
   }
 
   /*
@@ -108,15 +80,29 @@ object TrollService {
       return ArrayList
   */
   private def checkForTrollsMovement(controllingUnit: FieldUnit): util.ArrayList[FieldUnit] = {
-    val possibleCells = new util.ArrayList[FieldUnit]()
-    (4 to 7).foreach {
+    var possibleCells = new util.ArrayList[FieldUnit]()
+    (4 to 7).foreach{
       i => {
         val neighbor = controllingUnit.neighbors(i)
-        if (Cell.isCellEmpty(neighbor)) {
+        if (Cell.isCellEmpty(neighbor)){
           possibleCells.add(neighbor)
         }
       }
     }
     possibleCells
+  }
+
+  /*
+    Setting cells with dwarfs to empty and notify other player
+  */
+  def processTrollAttack(selectedCell: FieldUnit): Unit = {
+    var dwarfsToKill = new util.ArrayList[FieldUnit]()
+    for (neighbor <- selectedCell.neighbors if neighbor.cellType.equals(DWARF)) {
+      neighbor.cellType = EMPTY
+      dwarfsToKill.add(neighbor)
+    }
+    if (!dwarfsToKill.isEmpty){
+      ThudGame.fieldController ! DeleteFiguresMessage(dwarfsToKill)
+    }
   }
 }
